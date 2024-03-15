4c4ffa719d61e8d15e7c70820fd751e3310ba6f8
kasimaru13_Bastien <81258266+kasimaru13@users.noreply.github.com>
2024-03-15 13:20:43 +0100

    movie info

--- structured-data/movie_info_col.py
+++ structured-data/movie_info_col.py
@@ -32,50 +32,47 @@ for movie in movies:
     entry["title"] = movie["primaryTitle"]
 
     # Other titles
-    query = db["titles"].find({"mid": mid})
+    query = db["titles"].find({"mid": mid}, {"_id": 0, "region": 1, "title": 1})
     titles = []
-    title = {"region": "", "title": ""}
     for region in query:
-        title["region"] = region["region"]
-        title["title"] = region["title"]
-        titles.append(title)
+        titles.append(region)
     entry["titles"] = titles    
 
     # Genres
     query = db["genres"].find({"mid": mid})
     entry["genres"] = [element["genre"] for element in query]
 
-    # # Find principals
-    # principals = db["principals"].find({"mid": mid})
+    # Find principals
+    principals = db["principals"].find({"mid": mid})
 
-    # actors = []
-    # others = []
-    # for principal in principals:
-    #     person = {}
-    #     pid = principal["pid"]
-    #     person["pid"] = pid
+    actors = []
+    others = []
+    for principal in principals:
+        person = {}
+        pid = principal["pid"]
+        person["pid"] = pid
 
-    #     # Find name
-    #     person["name"] = db["persons"].find_one({"pid": pid})
+        # Find name
+        person["name"] = db["persons"].find_one({"pid": pid}, {"_id": 0, "primaryName": 1})
         
-    #     if principal["category"] == "actor":
-    #         # Find characters played
-    #         query = db["characters"].find({"mid": mid, "pid": pid})
-    #         characters = []
-    #         for character in query:
-    #             characters.append(character["name"])
+        if principal["category"] == "actor":
+            # Find characters played
+            query = db["characters"].find({"mid": mid, "pid": pid})
+            characters = []
+            for character in query:
+                characters.append(character["name"])
             
-    #         # Add actor to list
-    #         actors.append(person)
+            # Add actor to list
+            actors.append(person)
 
-    #     else:
-    #         person["category"] = principal["category"]
+        else:
+            person["category"] = principal["category"]
 
-    #         # Add to others
-    #         others.append(person)
+            # Add to others
+            others.append(person)
 
-    # entry["principals"] = others
-    # entry["actors"] = actors
+    entry["principals"] = others
+    entry["actors"] = actors
 
     # Rating
     rating = db["ratings"].find_one({"mid": mid})
@@ -87,7 +84,7 @@ for movie in movies:
     counter += 1
 
     # Send data by batch of 1000
-    if (counter == 1):
+    if (counter == 1000):
         collection.insert_many(entries)
         entries = []
         counter = 0
